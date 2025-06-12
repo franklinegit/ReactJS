eda29fac22124473327dfa012f5b4dd523b8d47f
franklinegit <oddfrankline@gmail.com>
2025-06-12 14:31:03 +0300

    Add spinner for loading animation

--- movieApp/src/App.jsx
+++ movieApp/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect }from 'react'
 import Search from './components/Search'
+import Spinner from './components/Spinner'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -77,7 +78,19 @@ const App = () => {
 				</header>
 
 				<section className="all-movies">
-					<h2>All Movies</h2>
+					<h2 className='mt-[40px]'>All Movies</h2>
+
+					{isLoading ? (
+						<Spinner />
+					) : errorMessage ? (
+						<p className='text-red-500'>{errorMessage}</p>
+					) : (
+						<ul>
+							{movieList.map((movie) => (
+								<p key={movie.id} className='text-white'>{movie.title}</p>
+							))}
+						</ul>
+					)}
 
 					{errorMessage && <p className='text-red-500'>{errorMessage}</p> }
 				</section>
