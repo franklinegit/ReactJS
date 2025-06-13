fd1c47436d4aa41c1be1637ab634e0ef6707dfe1
franklinegit <oddfrankline@gmail.com>
2025-06-13 17:22:14 +0300

    Set up the Zentry project

---
+++ Zentry/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='mt-5'>Zentry. Awwwards winning site. @site of the day</h1>
+      <h1>Zentry. Awwwards winning site. @site of the day</h1>
+      <h1>Zentry. Awwwards winning site. @site of the day</h1>
+      <h1>Zentry. Awwwards winning site. @site of the day</h1>
+    </main>
+  )
+}
+
+export default App
