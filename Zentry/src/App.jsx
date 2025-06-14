c7ff66a1f6a8f5cf4228166d30ff1cfb128b8fb1
franklinegit <oddfrankline@gmail.com>
2025-06-14 22:17:00 +0300

    Animate video frame clip on scroll

--- Zentry/src/App.jsx
+++ Zentry/src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <main className='min-h-screen w-screen relative overflow-x-hidden'>
       <Hero />
+
+      <section className='min-h-screen bg-blue-500'/>
     </main>
   )
 }
--- Zentry/src/components/Hero.jsx
+++ Zentry/src/components/Hero.jsx
@@ -1,10 +1,11 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from "react-icons/ti";
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Hero = () => {
 
@@ -13,7 +14,7 @@ const Hero = () => {
 	const [isLoading, setisLoading] = useState(true);
 	const [loadedVideos, setloadedVideos] = useState(0);
 
-	const totalVideos = 3;
+	const totalVideos = 4;
 	const nextVideoRef = useRef(null);  // For referencing a DOM element
 
 	const nextVideoIndex = (currentIndex % totalVideos) + 1;
@@ -26,10 +27,22 @@ const Hero = () => {
 
 	const getVideoSrc = (index) => `/videos/hero-${index}.mp4`;
 
+
 	const handleVideoLoad = () => {
 		setloadedVideos((prev) => prev + 1);
 	}
 
+	// Check loaded videos
+	useEffect(() => {
+		if (loadedVideos === totalVideos -1) {
+			setisLoading(false);
+		}
+	}, [loadedVideos]);
+
+	console.log(loadedVideos);
+	
+
+	// Animate Hero mini-vid on click
 	useGSAP(() => {
 		if (hasClicked) {
 			gsap.set('#next-video', {
@@ -57,9 +70,44 @@ const Hero = () => {
 		}
 	}, {dependencies:[currentIndex], revertOnUpdate: true});
 
+	// Animate video frame on scroll
+	useGSAP(() => {
+		gsap.fromTo('#video-frame', {
+			clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+			borderRadius: '0 0 0% 0%',
+			ease: 'power1.inOut',
+			// rotateX: '0',
+			// transformOrigin: 'center bottom',
+		}, {
+			clipPath: 'polygon(25% 0%, 75% 0%, 95% 80%, 5% 90%)',
+			borderRadius: '0 0 10% 10%',
+			// rotateX: '45',
+			// transformOrigin: 'center bottom',
+				scrollTrigger: {
+				trigger: '#video-frame',
+				start: 'bottom 90%',
+				end: 'bottom top',
+				scrub: true
+			}
+		});
+	});
+
   return (
 	<div className='relative size-screen overflow-x-hidden'>
-		<div id="video-frame" className='relative size-screen z-10 overflow-hidden rounded-lg bg-blue-75'>
+
+		{isLoading && (
+			<div className='flex-center size-screen absolute z-[100] overflow-hidden bg-violet-50'>
+				<div className="loading">
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+		)}
+
+		<div id="video-frame" className='relative size-screen z-10 overflow-hidden bg-blue-75'>
 			<div>
 				<div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
 					<div onClick={handleMiniVideoClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
@@ -97,7 +145,7 @@ const Hero = () => {
 			</div>
 
 			<h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>
-				G<b>a</b>ming
+				G<b>a</b>mi<b>n</b>g
 			</h1>
 
 			<div className='absolute top-0 left-0 size-full z-40'>
@@ -117,8 +165,23 @@ const Hero = () => {
 		</div>
 
 		<h1 className='special-font hero-heading absolute bottom-5 right-5 text-black'>
-			G<b>a</b>ming
+			G<b>a</b>mi<b>n</b>g
 		</h1>
+
+		<div className='absolute top-0 left-0 size-full'>
+			<div className='mt-24 px-5 sm:px-10'>
+				<h1 className='special-font hero-heading text-black0'>
+					Redefi<b>n</b>e
+				</h1>
+
+				<p className='text-black font-robert-regular mb-5 max-w-64'>
+					Enter the Metagame Layer <br />
+					Unleash the Play Economy
+				</p>
+
+				<Button title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1" />
+			</div>
+		</div>
 
 	</div>
   )
